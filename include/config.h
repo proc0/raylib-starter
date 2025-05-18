85ffe9c1e6811a53c3edff00d47d99fe526e29ab
proc0 <alanvx@gmail.com>
2025-05-18 01:19:13 -0700

    adjusting ui text

--- include/config.h
+++ include/config.h
@@ -2,5 +2,5 @@
 
 #define VERSION "1.0.0"
 #define MODE_DEBUG 
-#define DATE_BUILD "2025-05-14 16:30:21"
+#define DATE_BUILD "2025-05-15 15:12:32"
 #define PATH_RESOURCE "resource"
--- source/game.cpp
+++ source/game.cpp
@@ -39,14 +39,14 @@ void Game::Render() const {
     DrawRectangleGradientH(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, BLUE, GREEN);
     DrawCircleV(ballPosition, 40, ballColor);
     const char* countText = TextFormat("Count: %i", count);
-    DrawText(countText, 50, 50, 20, RED);
-    if (isCursorHidden == 1) DrawText("CURSOR HIDDEN", 20, 60, 20, RED);
-    else DrawText("CURSOR VISIBLE", 20, 60, 20, LIME);
+    DrawText(countText, 50, 50, 20, WHITE);
+    if (isCursorHidden == 1) DrawText("CURSOR HIDDEN", SCREEN_WIDTH/2-100, 60, 20, BLACK);
+    else DrawText("CURSOR VISIBLE", SCREEN_WIDTH/2-100, 60, 20, WHITE);
 }
 
 void Game::Loop() {
     Update();
-    
+
     BeginDrawing();
     Render();
     EndDrawing();
