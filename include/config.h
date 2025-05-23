a4dec0b04aab1cdcf7eaf662f0031f7410a4da02
proc0 <alanvx@gmail.com>
2025-05-22 22:00:15 -0700

    cleaning cmakelist

--- include/config.h
+++ include/config.h
@@ -2,5 +2,5 @@
 
 #define VERSION "1.0.0"
 #define MODE_DEBUG 
-#define DATE_BUILD "2025-05-22 21:35:43"
-#define PATH_RESOURCE "resource"
+#define DATE_BUILD "2025-05-22 21:55:18"
+#define PATH_RESOURCE ""
